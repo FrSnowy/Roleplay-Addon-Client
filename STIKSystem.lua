6138f82ce148f7e7877e75e0d3737db8510998d2
Albert <friendsnowy@gmail.com>
2019-09-20 01:29:17 +0500

    Version update

--- STIKSystem.lua
+++ STIKSystem.lua
@@ -152,7 +152,7 @@ function onDMSaySomething(prefix, msg, tp, sender)
             local progress = currentPlot.progress;
             local params = currentPlot.params;
             local flags = currentPlot.flags;
-            SendChatMessage('Версия: 2.0.1', "WHISPER", nil, sender);
+            SendChatMessage('Версия: 2.1.0', "WHISPER", nil, sender);
             SendChatMessage("STR:"..stats.str.." AG:"..stats.ag.." SNP:"..stats.snp.." MG:"..stats.mg.." BODY:"..stats.body.." MRL:"..stats.moral, "WHISPER", nil, sender);
             SendChatMessage("LVL:"..progress.lvl.." EXPR:"..progress.expr.."/"..(progress.lvl * 1000).." PNT:"..params.points, "WHISPER", nil, sender);
             SendChatMessage("HP:"..params.health.." SHLD:"..params.shield, "WHISPER", nil, sender);
